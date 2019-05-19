dc1c7bf8e4e46f7b39c827abb1995df86e1b085e
Rishi Banerjee <rishieric91@gmail.com>
2019-05-20 02:42:48 +0530

    Support for 3 types of comments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import FileManager
 
-fm = FileManager()
+file_type = input("Enter File Type you want to strip : ")
+
+fm = FileManager(file_type)
 fm.traverse_and_replace()
 print("Done!")
--- utils.py
+++ utils.py
@@ -3,22 +3,38 @@ import glob
 
 class FileManager:
 
-    def __init__(self):
-        self.txtFiles = glob.glob('**/*.txt', recursive=True)
+    def __init__(self, file_type):
+        self.file_type = '**/*.' + file_type
+        print("Extracting all .", self.file_type, " files")
+        self.txtFiles = glob.glob(self.file_type, recursive=True)
+        if not self.txtFiles:
+            print("Found None!")
         print(self.txtFiles)
 
     def traverse_and_replace(self):
         for txtFile in self.txtFiles:
             f = open(txtFile, 'r+')
             file_content = str(f.read())
-            print("File Content : ", str(file_content))
-            file_content = self.remove_comments(file_content)
+            if self.file_type == 'css':
+                file_content = self.remove_comments_css_style(file_content)
+            elif self.file_type == 'html':
+                file_content = self.remove_comments_html_style(file_content)
+            else:
+                file_content = self.remove_comments_c_style(file_content)
             f.close()
             f = open(txtFile, 'w')
             f.write(str(file_content).strip())
             f.close()
     
-    def remove_comments(self, string):
-        string = re.sub(re.compile("/\*.*?\*/",re.DOTALL ) ,"" ,string) # remove all occurrences streamed comments (/*COMMENT */) from string
-        string = re.sub(re.compile("//.*?\n" ) ,"" ,string) # remove all occurrence single-line comments (//COMMENT\n ) from string
+    def remove_comments_c_style(self, string):
+        string = re.sub(re.compile("/\*.*?\*/",re.DOTALL ) ,"" ,string)
+        string = re.sub(re.compile("//.*?\n" ) ,"" ,string) 
+        return string
+    
+    def remove_comments_css_style(self, string):
+        string = re.sub(re.compile("(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)", re.DOTALL), "", string)
+        return string
+
+    def remove_comments_html_style(self, string):
+        string = re.sub(re.compile("<!--(.*?)-->"), "", string)
         return string
